b8ad22680917adcfa8f8b805b8bc008ddfc0f55f
jonathan.lee <jonathan.lee@slalom.com>
2023-03-12 14:56:10 -0400

    (chore) Added styled-components.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,19 +1,16 @@
 import React, { useEffect } from 'react';
 import type { AppProps } from 'next/app';
 import { MainMenu } from '../components/main-menu/main-menu';
+import styled from 'styled-components';
 
+const Foo = styled.div`
+  background: red;
+`;
 const MyApp = ({ Component, pageProps }: AppProps): React.ReactElement => {
-  useEffect(() => {
-    const style = document.getElementById('server-side-styles');
-
-    if (style) {
-      style.parentNode.removeChild(style);
-    }
-  }, []);
-
   return (
     <>
       <MainMenu />
+      <Foo>Foo</Foo>
       <Component {...pageProps} />;
     </>
   );
